9f62d1228184af1652987c2784686717c2546504
Savelius Milla <s2401247@edu.bc.fi>
2026-02-10 14:35:33 +0200

    ruoka annos
    tein hieman lisää

---
+++ Pelikehitys-OO/Pelikehitys-OO/Ruoka-annos/RuokaAnnosProgram.cs
@@ -0,0 +1,108 @@
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
+
+namespace Ruoka_annos
+{
+    public enum Paaraaka
+    {
+        nautaa,
+        kanaa,
+        kasviksia
+    }
+    public enum Lisuke
+    {
+        perunaa,
+        riisiä,
+        pastaa
+    }
+    public enum Kastike
+    {
+        curry,
+        hapanimelä,
+        pippuri,
+        chili
+    }
+  
+    public class Ateria
+    {
+        public Paaraaka paaraaka;
+        public Lisuke lisuke;
+        public Kastike kastike;
+
+        public Ateria(Paaraaka paaraaka,  Lisuke lisuke, Kastike kastike)
+        {
+            this.paaraaka = paaraaka;
+            this.lisuke = lisuke;
+            this.kastike = kastike;
+        }
+
+        public Paaraaka AnnaPaa()
+        {
+            return paaraaka; //voi olla myös return this.paaraaka
+        }
+
+        public override string ToString()
+        {
+            return $"{paaraaka}, {lisuke} ja {kastike}";
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Paaraaka valittuP = Paaraaka.kanaa;
+            Lisuke valittuL = Lisuke.perunaa;
+            Kastike valittuK = Kastike.chili;
+
+            // kysele käyttäjältä annoksen osat
+            //jos haluaisin 3 ateriaa pitäisi tehdä for loop ja lista
+            //pitäisi myös selvittää miten katsoa enum lista ja muuttaa ne otettaviksi stringeiksi
+            Console.Write("Pääraaka-aine (nautaa, kanaa ja kasviksia): ");
+            string valittu = Console.ReadLine();
+            TarkistaPaa(valittuP, valittu);
+
+            Console.Write("Lisukkeet (perunaa, riisiä ja pastaa): ");
+            valittu = Console.ReadLine();
+
+            Console.Write("Kastike (pippuri, chili, tomaatti ja curry): ");
+            valittu = Console.ReadLine();
+
+
+            //näytä käyttäjän annos
+            Ateria a = new Ateria(valittuP, valittuL, valittuK);
+            Console.WriteLine(a);
+        }
+
+        public static Paaraaka TarkistaPaa(Paaraaka paaraaka, string valinta)
+        {
+            switch (valinta)
+            {
+                case "nautaa":
+                    return Paaraaka.nautaa;
+                case "kanaa":
+                    return Paaraaka.kanaa;
+                case "kasviksia":
+                    return Paaraaka.kasviksia;
+                default:
+                    Console.WriteLine("Ei ole listalla. Anamme kanaa.");
+                    return paaraaka;
+            }
+        }
+        public static Lisuke TarkistaLisuke(Lisuke lisuke, string valinta)
+        {
+            switch (valinta)
+            {
+                case "perunaa":
+                    return Lisuke.perunaa;
+                case "riisiä":
+                    return Lisuke.riisiä;
+                case "pastaa":
+                    return Lisuke.pastaa;
+                default:
+                    Console.WriteLine("Ei ole listalla. Anamme kanaa.");
+                    return lisuke;
+            }
+        }
+    }
+}
