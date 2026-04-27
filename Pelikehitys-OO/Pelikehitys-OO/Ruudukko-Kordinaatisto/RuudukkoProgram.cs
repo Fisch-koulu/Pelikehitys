d00ec77ad7339d55f81e87118e1df4cb3a41ebd2
Savelius Milla <s2401247@edu.bc.fi>
2026-04-27 10:39:54 +0300

    Ruudukko kordinaatisto
    teht 6 tehty
    laitoin koordinaatit väärinpäin :P

---
+++ Pelikehitys-OO/Pelikehitys-OO/Ruudukko-Kordinaatisto/RuudukkoProgram.cs
@@ -0,0 +1,59 @@
+﻿namespace Ruudukko_Kordinaatisto
+{
+    internal class RuudukkoProgram
+    {
+        static void Main(string[] args)
+        {
+            Koordinaatti yksi = new Koordinaatti(0,0);
+            Koordinaatti kaksi = new Koordinaatti(-1,0);
+
+            yksi.Tarkistus(kaksi);
+        }
+    }
+
+    public struct Koordinaatti
+    {
+        private int x;
+        private int y;
+
+        public int X { get { return x; } private set { x = value; } }
+        public int Y { get { return y; } private set { y = value; } }
+
+        public Koordinaatti(int x, int y) 
+        { 
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"{x},{y}";
+        }
+
+        //tarkistaa onko koordinaatit vieressä, ja palauttaa true/false
+        public bool Tarkistus(Koordinaatti og)
+        {
+            //ensin tarkistaa onko koordinaatti sama
+            if (this.x == og.x && this.y == og.y) 
+            {
+                Console.WriteLine($"Anettu koordinaatti {this.ToString()} on koordinaatissa {og.ToString()}.");
+                return true;
+            }
+            //sitten katsoo onko x 1 isompi tai pienempi välissä
+            else if (this.x <= og.x + 1 && this.x >= og.x -1)
+            {
+                if (this.y <= og.y + 1 && this.y >= og.y - 1)
+                {
+                    //sitten katsoo onko y 1 isompi tai pienempi välissä
+                    Console.WriteLine($"Anettu koordinaatti {this.ToString()} on koordinaatin {og.ToString()} vieressä.");
+                    return true;
+                }
+                //tulemme vain tähän jos, jos y ei ei ole 1 isompi tai pienempi välissä
+                Console.WriteLine($"Anettu koordinaatti {this.ToString()} ei ole koordinaatin {og.ToString()} vieressä.");
+                return false;
+            }
+            Console.WriteLine($"Anettu koordinaatti {this.ToString()} ei ole koordinaatin {og.ToString()} vieressä.");
+            return false;
+        }
+    }
+}
